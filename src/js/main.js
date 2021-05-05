4962cfe62dfaf994aa7857488c5b3362165faaab
ulugbekwebp <ulugbekwebp@gmail.com>
2021-05-05 14:58:01 +0500

    login-+

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,10 @@ var panda_video = document.querySelector(".hero__panda-video");
 var panda_img = document.querySelector(".hero__img");
 var header__item_btn_contact_us = document.querySelector(".header__item-btn-contact-us");
 var modal__contact_us = document.querySelector(".modal__contact-us");
-
+var modal__body_label = document.querySelector(".modal__body-label");
+var modal__body_label_second = document.querySelector(".modal__body-label-second");
+var modal__body_item = document.querySelector(".modal__body-item-first");
+var modal__body_item_second = document.querySelector(".modal__body-item-second");
 
 panda.addEventListener("click", function(){
   panda_video.classList.toggle('hero__panda-video-active');
@@ -13,3 +16,16 @@ panda.addEventListener("click", function(){
 header__item_btn_contact_us.addEventListener("click", function(){
   modal__contact_us.classList.toggle("modal__contact-us-active");
 })
+
+modal__body_label.addEventListener("click", function(){
+  modal__body_item.classList.add("modal__body-item-active");
+  modal__body_item_second.classList.remove("modal__body-item-active");
+  
+})
+modal__body_label_second.addEventListener("click", function(){
+  modal__body_item.classList.remove("modal__body-item-active");
+  modal__body_item_second.classList.add("modal__body-item-active");
+})
+
+
+
